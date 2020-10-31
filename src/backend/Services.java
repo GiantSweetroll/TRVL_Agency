76c6256a24cdfd54dde862c6239eb6a89a95335b
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-10-31 15:30:16 +0700

    Added hello world

--- src/backend/Services.java
+++ src/backend/Services.java
@@ -3,5 +3,10 @@ package backend;
 /**
  * Created by user on 10/28/2020.
  */
-public class Services {
+public class Services
+{
+    public static void main (String args[])
+    {
+        System.out.println("Hello dfdfgb");
+    }
 }
