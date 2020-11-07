8b932e3f6da0f15ba782ec61ca93243385628e9e
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-11-07 17:38:04 +0700

    Create Destination.java

---
+++ src/models/Destination.java
@@ -0,0 +1,42 @@
+package models;
+
+public class Destination
+{
+    //Fields
+    private String name;
+
+    //Constructor
+    /**
+     * Instatiates an object of Destination. The name field will be an empty String.
+     */
+    public Destination()
+    {
+        this.name = "";
+    }
+    /**
+     * Instantiates an object of Destination with the specified name.
+     *
+     * @param name
+     */
+    public Destination(String name, double cost)
+    {
+        this.name = name;
+    }
+
+    //Public Methods
+    /**
+     * @return a String of the destination's name
+     */
+    public String getName()
+    {
+        return name;
+    }
+    /**
+     * Set the name of the destination.
+     * @param name
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
