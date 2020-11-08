045ebffce6b4d8c7e1a3008c1064ca7762cb9845
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-11-08 16:30:53 +0700

    Created methods that uses Dijkstra algorithm to calculate shortest distance and cost

--- src/backend/Services.java
+++ src/backend/Services.java
@@ -1,10 +1,36 @@
 package backend;
 
+import models.DestinationGraph;
+
 /**
  * Created by user on 10/28/2020.
  */
 public class Services
 {
+    /**
+     * Get the shortest distance between a destination and all other destinations.
+     * @param destinations - an object of DestinationGraph
+     * @param reference - index of the source destination.
+     * @return an array of double that signifies the shortest distance from that destination to the source destination.
+     */
+    public double[] getShortestDistances(DestinationGraph destinations, int reference)
+    {
+        Dijkstra d = new Dijkstra(destinations.getDistanceMatrixArray());
+        return d.compute(reference);
+    }
+
+    /**
+     * Get the lowest cost between a destination and all other destinations.
+     * @param destinations - an object of DestinationGraph
+     * @param reference - index of the source destination.
+     * @return an array of double that signifies the lowest cost from that destination to the source destination.
+     */
+    public double[] getCheapestCosts(DestinationGraph destinations, int reference)
+    {
+        Dijkstra d = new Dijkstra(destinations.getCostMatrixArray());
+        return d.compute(reference);
+    }
+
     public static void main (String args[])
     {
         System.out.println("Hello dfdfgb");
