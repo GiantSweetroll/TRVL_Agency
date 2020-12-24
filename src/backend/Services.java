997ba6007fa0c1a650c81403a4c0ca00e8410a35
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-25 02:03:33 +0700

    Added some utility methods

--- src/backend/Services.java
+++ src/backend/Services.java
@@ -3,7 +3,11 @@ package backend;
 import models.Destination;
 import models.DestinationGraph;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by user on 10/28/2020.
@@ -34,6 +38,70 @@ public class Services
         Dijkstra d = new Dijkstra(destinations.getCostAdjMatrixArray());
         BellmanFord b = new BellmanFord(destinations.getCostRowGraphArray(), destinations.getDestinationNames().size());
         return d.compute(reference);
+    }
+
+    /**
+     * Convert a 2D List to a 2D Array
+     * @param list
+     * @return a 2D Array
+     */
+    public static double[][] convert2DListToArray(List<ArrayList<Double>> list)
+    {
+        int size = list.size();
+        double[][] arr = new double[size][size];
+
+        for (int i=0; i<size; i++)
+        {
+            for (int a=0; a<size; a++)
+            {
+                arr[i][a] = list.get(i).get(a);
+            }
+        }
+
+        return arr;
+    }
+
+    /**
+     * Converts adjacency matrix to row graph matrix (in 2D array format)
+     * @param adjMatrix - a List<ArrayList<Double>> object
+     * @return a double[][3] object
+     */
+    public static double[][] convertAdjMatrixToRowGraphMatrix(List<ArrayList<Double>> adjMatrix)
+    {
+        int size = adjMatrix.size();
+        List<ArrayList<Double>> list = new ArrayList<>(); //Each connection will have 3 items: vertex 1 index, vertex 2 index, weight
+
+        //Convert adjacency matrix to adjacency list
+        for (int i=0; i<size; i++)
+        {
+            for (int a=0; a<size; a++)
+            {
+                double val = adjMatrix.get(i).get(a);
+                if (val != 0d)
+                {
+                    //Establish connection
+                    ArrayList<Double> subList = new ArrayList<>();
+                    subList.add((double)i);     //Vertex 1 index
+                    subList.add((double)a);     //Vertex 2 index
+                    subList.add(val);           //Weight
+
+                    //Add to list
+                    list.add(subList);
+                }
+            }
+        }
+
+        //Convert to primitive 2D array
+        double[][] arr = new double[list.size()][3];   //Each connection will have 3 items: vertex 1 index, vertex 2 index, weight
+        for (int i=0; i<list.size(); i++)
+        {
+            for (int a=0; a<3; a++)
+            {
+                arr[i][a] = list.get(i).get(a);
+            }
+        }
+
+        return arr;
     }
 
     public static void main (String args[])
